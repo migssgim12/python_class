3d1677f1f65824804e47ec15b0aafb971ed9435c
Michael Poehner <michaeljpoehner@gmail.com>
2017-05-30 11:51:15 -0700

    Didn't commit last class. We did Sass and styling on Thursday. Today we started Web services, where we parsed data from an api and returned user results.

---
+++ 3_AppliedPython/random_users.py
@@ -0,0 +1,28 @@
+"""
+Fun with Networks
+"""
+
+import requests
+
+
+
+def get_users():
+    response = requests.get('http://api.randomuser.me/?results=5')
+    users = response.json().get('results')
+    return users
+
+
+def display_users():
+    users = get_users()
+
+    for user in users:
+        name_data = user.get('name')
+        first_name, last_name = name_data['first'], name_data['last']
+        email = user.get('email')
+        user_name = user.get('login')['username']
+        reg_name = user.get('registered')
+        date_of_birth = user.get('dob')
+        print(f'Name: {first_name} {last_name}\nEmail: {email} \nUsername: {user_name} \nRegistration Date: {reg_name} \nBirth Date: {date_of_birth}\n')
+
+
+display_users()
